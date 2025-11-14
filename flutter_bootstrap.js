b6fb3de38299f7427b86ff504790761b644e7bef
vattana198 <136791404+vattana198@users.noreply.github.com>
2025-11-14 18:53:08 +0700

    fixed all

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"010c8a806bccad64ab9972286b85dd24ca9844
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1258259026"
+    serviceWorkerVersion: "3171109594"
   }
 });
